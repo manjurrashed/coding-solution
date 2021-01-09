69634f1572dd7afa80057e31fcf5decef52b3505
Manjur Khan <manjur>
2021-01-08 19:53:57 -0800

    linked list

---
+++ c_plus_plus/linkedList/82_LC_Remove_duplicates_from_sorted_listII.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode dummy;
+        dummy.next = head;
+        ListNode *prev = &dummy;
+        ListNode *cur = head;
+        
+        while(cur) {
+            if (cur->next && cur->val == cur->next->val) {
+                cur = cur->next;
+            } else {
+                if (prev->next != cur) {
+                    prev->next = cur->next;
+                    
+                } else {
+                    prev = cur;
+                }
+                cur = cur->next;
+            }
+        }
+        return dummy.next;
+    }
+};
