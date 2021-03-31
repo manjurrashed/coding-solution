25de544f0c6f0bc8e85544511f85f55258f39372
Manjur Khan <manjur>
2021-03-30 22:44:01 -0700

    update

--- c_plus_plus/linkedList/82_LC_Remove_duplicates_from_sorted_listII.cpp
+++ c_plus_plus/linkedList/82_LC_Remove_duplicates_from_sorted_listII.cpp
@@ -11,6 +11,7 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
+#if 0
         ListNode dummy;
         dummy.next = head;
         ListNode *prev = &dummy;
@@ -30,5 +31,28 @@ public:
             }
         }
         return dummy.next;
+#endif
+ 	ListNode dummy(0);
+        dummy.next = head;
+        
+        ListNode *slow = &dummy;
+        ListNode *fast = head;
+        while (fast) {
+            if (fast->next && fast->val == fast->next->val) {
+                while (fast->next && fast->val == fast->next->val) {
+                    ListNode *temp = fast;
+                    fast = fast->next;
+                    delete temp;
+                }
+                slow->next = fast->next;
+                delete fast;
+                fast = slow->next;
+            } else {
+                slow = fast;
+                fast = fast->next;
+            }
+        }
+        return dummy.next;
+
     }
 };
