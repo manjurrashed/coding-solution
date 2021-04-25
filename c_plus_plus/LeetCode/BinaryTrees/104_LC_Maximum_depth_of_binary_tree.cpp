2d5869505c96743c70b6014e425c808241efd0ab
Manjur Khan <manjur>
2021-04-25 00:01:12 -0700

    update

--- c_plus_plus/LeetCode/BinaryTrees/104_LC_Maximum_depth_of_binary_tree.cpp
+++ c_plus_plus/LeetCode/BinaryTrees/104_LC_Maximum_depth_of_binary_tree.cpp
@@ -22,9 +22,18 @@ public:
         helper(root->right, depth, max_depth);
     }
     int maxDepth(TreeNode* root) {
+	#if 0
         if (!root) return 0;
         int max_depth = INT_MIN;
         helper(root, 0, max_depth);
         return max_depth;
+	#endif
+	#if 1
+        if (!root)
+            return 0;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return max(left, right) + 1;
+        #endif
     }
 };
