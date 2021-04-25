52c12f00156563b890b78ca4de6c91e4ee542353
Manjur Khan <manjur>
2021-04-25 00:25:55 -0700

    update

---
+++ c_plus_plus/LeetCode/BinaryTrees/98_LC_Validate_Binary_Search_Tree.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool helper(TreeNode* root, long lv, long rv) {
+        if (!root)
+            return true;
+        if (lv >= root->val || rv <= root->val)
+            return false;
+        return helper(root->left, lv, root->val) && helper(root->right, root->val, rv);
+        
+    }
+    bool isValidBST(TreeNode* root) {
+        return helper(root, LONG_MIN, LONG_MAX);
+    }
+};
+
